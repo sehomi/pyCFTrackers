fb36eacf4a8aff3e218f6dd1f677ba0eadd5e9d2
sehomi <sehomi755@gmail.com>
2022-06-23 12:17:12 +0000

    dimp50 and park 6 done

--- examples/viotdataset_config.py
+++ examples/viotdataset_config.py
@@ -62,7 +62,7 @@ class VIOTDatasetConfig:
                   "cup_1.5HZ":[0.3, 0], "cup_1.8HZ":[0.3, 0], "cup_2.1HZ":[0.3, 0], \
                   "cup_3.2HZ":[0.3, 0], "park_mavic_1":[0.2, 1], "park_mavic_2":[0.2, 1], \
                   "park_mavic_3":[0.2, 1], "park_mavic_4":[0.3, 1], "park_mavic_5":[0.3, 1], \
-                  "park_mavic_6":[0.3, 1], "park_mavic_7":[0.2, 1], "soccerfield_mavic_3":[0.3, 1], \
+                  "park_mavic_6":[0.2, 1.2], "park_mavic_7":[0.2, 1], "soccerfield_mavic_3":[0.3, 1], \
                   "soccerfield_mavic_4":[0.3, 1]},
         "PRDIMP50":{"cup_0.5HZ":[0.1, 0], "cup_0.9HZ":[0.1, 0], "cup_1.1HZ":[0.1, 0], \
                   "cup_1.5HZ":[0.1, 0], "cup_1.8HZ":[0.1, 0], "cup_2.1HZ":[0.1, 0], \
